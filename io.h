b56115ac8839e07ed68d142535d3cea5593d72da
Ryan H. Lewis <me@ryanlewis.net>
2013-11-14 16:35:18 -0800

    distributed stuff and instructions for io

--- io.h
+++ io.h
@@ -3,6 +3,62 @@
 #include <sstream>
 namespace ublas = boost::numeric::ublas;
 namespace t10 {
+	template< typename String, typename Matrix, typename Communicator>
+	void read_matrix( const String & filename, Matrix & M, const Communicator & world){
+		//TODO: Implement reading
+		//world.rank() is the processors # 
+		//out of world.size() total processors	
+		//initially M is not the appropriate size
+		// 1. open the file in filename
+		// 2. we need to be able to know the matrix size in advance, say it is n x n
+		// 3. M.resize(block_size(n),block_size(n))  
+		// 4. read in data into M
+	}
+	template<typename Variable_map>
+	void process_args( int & argc, char *argv[],Variable_map & vm){
+	  //parse command line options
+	  po::options_description desc( "Usage: tp [options] input-file");
+	  desc.add_options()
+	  ( "help", "Display this message")
+	  ( "input-file", "input matrix file to parse");
+	  po::positional_options_description p;
+	  p.add( "input-file",1);
+	  po::store( po::command_line_parser( argc,argv)
+	            .options( desc)
+	            .positional( p)
+	            .run(),
+	             vm);
+	  po::notify( vm);
+	  if ( vm.count( "help")){
+	        std::cout << desc << std::endl;
+	        std::exit( 0);
+	  }
+	  if ( vm.count( "input-file") != 1){
+	        std::cout << desc << std::endl;
+	        std::exit( -1);
+	  }
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	template< typename Matrix>
 	std::string print_matrix( const Matrix & M, std::size_t p=7){
 		std::string str;
--- qr_distributed.cpp
+++ qr_distributed.cpp
@@ -1,78 +1,41 @@
-
+//STL
 #include <iostream>
-#include "qr_algorithm_serial.h"
+#include <algorithm>
+#include <string>
+
+//BOOST UBLAS
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/io.hpp>
-#include <algorithm>
 #include <boost/numeric/ublas/banded.hpp> //for banded_adaptor
 
+//BOOST PROGRAM OPTIONS
+#include <boost/program_options.hpp>
+
+//BOOST MPI
+#include <boost/mpi/environment.hpp>
+#include <boost/mpi/communicator.hpp>
+
+//PROJECT
+#include "qr_algorithm_serial.h"
+
 namespace ublas = boost::numeric::ublas;
+namespace po = boost::program_options;
 
 typedef ublas::vector< double > Vector;
 typedef ublas::matrix< double> Matrix;
 typedef ublas::zero_matrix< double> Zero_matrix;
 typedef ublas::zero_vector< double> Zero_vector;
 typedef typename ublas::diagonal_adaptor< Matrix> Diagonal_adapter;
-int main( int argc, char * argv[]){
-	const double test_case[3][3] = 
-       {{5,3,3},
-	{8,7,4},
-	{4,3,8}};
-	Matrix M0(0,0);
-	Matrix M1(1,1);
-	M1(0,0) = 42;
-	Matrix M2(2,2);
-	M2(0,0) = 42;  
-	M2(0,1) = 24; 
-	M2(1,0) = 69; 
-	M2(1,1) = 12;
-	t10::qr(M1);
-	t10::qr(M2);
-	std::cout << t10::print_matrix(M1) << std::endl << t10::print_matrix(M2) << std::endl;
-	const double correct_hess[3][3] = 
-  	{{ 5.000000000000000,   -4.024922359499620,    1.341640786499874},
-   	 {-8.944271909999159,   10.000000000000000,   -3.000000000000000},
-   	 { 0.000000000000000,   -2.000000000000000,    5.000000000000000}};
 
-	const double vector_test[3] = {3,2,1};
-   	const double correct_house[3] = {1.00000000000000,-2.69666295470958,-1.34833147735479};
-	const double correct_beta =  0.198216274262727;
-	const double correct_eigs[3] = {14.6235, 1.0000, 4.3765};
-	//define matrix type (use boost)
-	//read input with boost program options
-	//boost supports specializations for banded, triangular, etc, see:
-	//http://www.boost.org/doc/libs/1_54_0/libs/numeric/ublas/doc/index.htm
-	const std::size_t n = 3;
-	Matrix M(n,n);
-	Matrix HM(n,n);
-	Matrix A(n,n);
-	Vector V(n);
-	Vector CV(n);
-	Vector E(n);
-	for (unsigned int i = 0; i < M.size1(); ++i){
-		V(i) = vector_test[i];
-		CV(i) = correct_house[i];
-		E(i) = correct_eigs[i];
-		for(unsigned int j = 0; j < M.size2(); ++j){
-			M(i,j) = test_case[i][j];
-			A(i,j) = correct_hess[i][j];
-		}
-	}
-	HM = M;
-	//Householder Vector Test
-	std::cout << "Input: " << V << std::endl;
-	t10::compute_householder_vector(V);
-	double beta = V(0); V(0) = 1;
-	std::cout << "beta: " << beta << " correct: " << correct_beta << std::endl;
-	std::cout << "V: " << V << " correct V: " << CV << std::endl;
-	std::cout << " ------------------------------------------------- " << std::endl;	
-	//Hessenberg Reduction Test
-	std::cout << "Input Matrix: " << t10::print_matrix(HM)<< std::endl;
-	t10::hessenberg(HM);
-	std::cout << "M = " << t10::print_matrix(HM) << std::endl;
-	std::cout << "A = " << t10::print_matrix(A) << std::endl;
-	t10::qr(M);
-	Diagonal_adapter D(M);
-	std::cout << "D = " << t10::print_matrix(D) << std::endl;
-	std::cout << "Correct Eigs: " << E << std::endl; 	
+int main( int argc, char * argv[]){
+	//initialize mpi
+	mpi::environment env(argc, argv);
+  	mpi::communicator world;
+	//read input
+	po::variables_map vm;
+	t10::process_args( argc, argv, vm);
+	std::string filename( vm[ "input-file"].as< std::string>());
+	Matrix M;
+	t10::read_matrix( filename, M, world);
+//	t10::qr(M, world);
 }
