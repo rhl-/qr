0cb22e423604d8943286a970b124f58f6be6b53f
Ryan H. Lewis <me@ryanlewis.net>
2013-11-14 16:07:03 -0800

    forgot qr_alg

--- qr_algorithm.h
+++ qr_algorithm.h
@@ -2,91 +2,17 @@
 #define QR_ALGORITHM_H
 #define QR_ITERATION_OUTPUT
 //#define DEBUG_QR_ITERATION //comment me out to turn off debug mode
-#include <iomanip>
 #include <boost/timer.hpp>
 #include <boost/numeric/ublas/banded.hpp> //for banded_adaptor
 #include <boost/numeric/ublas/matrix.hpp> //for slices
 #include <boost/numeric/ublas/matrix_proxy.hpp> //for slices
 #include <boost/numeric/ublas/vector_proxy.hpp> //for slices
-#include <boost/numeric/ublas/io.hpp> //io
+#include "io.h"
 #ifdef DEBUG_QR_ITERATION
-#include <sstream>
 #endif //endif DEBUG_QR_ITERATION
 namespace ublas = boost::numeric::ublas;
 
 namespace t10 {
-	template< typename Matrix>
-	std::string print_matrix( const Matrix & M, std::size_t p=7){
-		std::string str;
-		std::stringstream ss;
-		ss.setf( std::ios::fixed, std:: ios::floatfield );
-		ss << std::setw( p) << std::setprecision( p) <<  M;
-		str = ss.str();
-		//kill size thing
-		std::size_t lefts = str.find(std::string("["));
-		std::size_t rights = str.find(std::string("]"));
-		str.replace(lefts,rights-lefts+1,"");
-		
-		//start with new line	
-		std::size_t found = str.find(std::string("(("));
-		str.replace(found,2,std::string("\n"));
-		//end with new line
-		found = str.find(std::string("))"));
-		str.replace(found,2,std::string("\n"));
-	
-		//end of each row
-		found = str.find(std::string("),"));
-		while( found != std::string::npos){
-			str.replace(found,2,std::string("\n"));
-			found = str.find(std::string("),"));
-		}
-		//remove beginning of row
-		found = str.find(std::string("("));
-		while( found != std::string::npos){
-			str.replace(found,1,std::string(""));
-			found = str.find(std::string("("));
-		}
-		//replace , with tab
-		found = str.find(std::string(","));
-		while( found != std::string::npos){
-			str.replace(found,1,std::string("\t"));
-			found = str.find(std::string(","));
-		}
-		
-		return str;
-	}
-
-
-
-	
-	template< typename Matrix>
-	std::string print_matrix( const Matrix & M, bool matlab){
-		std::string str;
-		std::stringstream ss;
-		ss << M;
-		str = ss.str();
-		std::size_t lefts = str.find(std::string("["));
-		std::size_t rights = str.find(std::string("]"));
-		str.replace(lefts,rights-lefts+1,"");
-		std::size_t found = str.find(std::string("(("));
-		str.replace(found,2,std::string("["));
-		found = str.find(std::string("))"));
-		str.replace(found,2,std::string("]"));
-		found = str.find(std::string("),"));
-		while( found != std::string::npos){
-			str.replace(found,2,std::string(";"));
-			found = str.find(std::string("),"));
-		}
-
-		found = str.find(std::string("("));
-		while( found != std::string::npos){
-			str.replace(found,1,std::string(""));
-			found = str.find(std::string("("));
-		}
-
-
-		return str;	
-	}
 	//GVL Section 5.1.9
 	template< typename Value>
 	void compute_givens(const Value & a, const Value & b, Value & c, Value & s){
