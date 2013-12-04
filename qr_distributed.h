320395770aeb20b4b87cb3dd797dff18a9e76ee1
Ryan H. Lewis <me@ryanlewis.net>
2013-12-03 17:57:09 -0800

    removed irrelevant code

--- qr_distributed.h
+++ qr_distributed.h
@@ -19,129 +19,7 @@
 namespace ublas = boost::numeric::ublas;
 namespace t10 {
 	namespace parallel {
-	//GVL Section 5.1.9
-	template< typename Value>
-	void compute_givens(const Value & a, const Value & b, 
-			    Value & c, Value & s){
-			if (b == 0) { c = 1; s = 0; return; }
-			if( std::fabs(a) < std::fabs(b)){
-				const Value tau = -a/b;
-				s = 1/ std::sqrt(1 + std::pow(tau, 2));
-				c = tau * s;
-				return;
-			}
-			const Value tau = -b/a;
-			c = 1/ std::sqrt(1 + std::pow(tau, 2));
-			s = tau * c;
-	}
-
-	template< typename T>
-	int sign( const T & x){ return (x < 0) ? -1 : 1; }
 	
-	template< typename Value>
-	Value encode_givens( const Value & c, const Value & s){
-			if (c == 0) { return 1; }
-			if (std::fabs(s) < std::fabs(c)) { return sign(c)*s/2; }
-			return 2*sign(s)/c;
-	}
-	template< typename Value>
-	void decode_givens( const Value & rho, Value & c, Value & s ){
-		if (rho == 1) { c = 0; s = 1; return; }
-		if (std::fabs(rho) < 1) { 
-			s = 2*rho; 
-			c = std::sqrt(1 -std::pow(s,2)); 
-			return;
-		}
-		c = 2/rho; s = std::sqrt(1-std::pow(c,2));
-	} 
-	
-	template< typename Matrix>
-	typename Matrix::value_type apply_givens_left( Matrix & M, 
-						       const std::size_t i, 
-						       const std::size_t k){
-		typedef typename Matrix::value_type Value;
-		Value c=0.0, s=0.0;
-		compute_givens(M(i,i),M(k,i),c,s);
-		for(std::size_t j = i; j < M.size2(); ++j){
-			const double ti = M(i,j);
-			const double tj = M(k,j);
-			M(i,j) = c*ti - s*tj;
-			M(k,j) = s*ti + c*tj;
-		}
-		M(k,i) = 0;
-		return encode_givens(c,s);
-	}
-	template< typename Matrix>
-	void apply_givens_right( Matrix & M, 
-				 const typename Matrix::value_type rho, 
-				 const std::size_t i, const std::size_t k){
-		typename Matrix::value_type c=0.0,s=0.0;
-		decode_givens(rho, c,s);
-		std::size_t max_iter = std::min(std::max(i,k)+1,M.size2());
-		for(std::size_t j = 0; j < max_iter; ++j){
-			const double t1 = M(j,i);
-			const double t2 = M(j,k);
-			M(j,i) = c*t1 - s*t2;
-			M(j,k) = s*t1 + c*t2;
-		}
-	}
-
-	template< typename Matrix>
-	void qr_iteration( Matrix & H, double tol=1e-16){
-		#ifdef QR_ITERATION_OUTPUT
-		std::cout << "H = " << print_matrix( H) << std::endl;
-		#endif	
-		std::size_t n = H.size1();
-		typedef typename Matrix::value_type Value;
-		typedef typename ublas::diagonal_adaptor< Matrix> 
-							Diagonal_adapter;
-		typedef typename ublas::scalar_matrix< Value> Scalar;
-		typedef ublas::diagonal_adaptor< Scalar> Diagonal_scalar;
-		std::vector< typename Matrix::value_type> givens(n-1, 0.0); 
-		do{
-			const Value a = H(n-2,n-2)+H(n-1,n-1);
-			const Value b = std::sqrt(4*H(n-1,n-2)*H(n-2,n-1) + 
-					std::pow((H(n-2,n-2)-H(n-1,n-1)),2));
-			const Value e1 = (a+b)/2, e2 = (a-b)/2;
-			const Value shift = (std::fabs(e1- H(n-1,n-1)) < 
-					std::fabs(e2 - H(n-1,n-1)))? e1 : e2;
-			
-			Scalar mu_s(n,n,shift);
-			const Diagonal_scalar mu( mu_s );
-			Diagonal_adapter D(H);
-			D -= mu;
-			for (std::size_t i = 0; i < n-1; ++i){ 
-				givens[i] = apply_givens_left(H,i,i+1); 
-			}
-			
-			#ifdef DEBUG_QR_ITERATION
-				std::cout << "Left Apply: H = " 
-					<< print_matrix( H) << std::endl;
-			#endif //DEBUG_QR_ITERATION
-			
-			for (std::size_t i = 0; i < n-1; ++i){ 
-				apply_givens_right(H,givens[i],i,i+1); 	
-			}
-			
-			#ifdef DEBUG_QR_ITERATION
-				std::cout << "Right Apply H = " 
-					<< print_matrix( H) << std::endl;
-			#endif //DEBUG_QR_ITERATION
-			
-			//Unshift
-			D += mu;
-			
-			#ifdef QR_ITERATION_OUTPUT
-			std::cout.precision( 7);
-			std::cout.setf( std::ios::fixed, std:: ios::floatfield);
-			std::cout << "---- Shift: " << std::setw( 10) << shift  
-			<< std::endl;
-			std::cout << "---- Error: " << std::setw( 10)  
-			<< std::fabs(H(n-1,n-2)) << std::endl;
-			#endif //DEBUG_QR_ITERATION
-		} while( std::fabs(H(n-1,n-2)) > tol);
-		
-	}
 	template< typename Vector, typename Communicator>
 	typename Vector::value_type compute_householder_vector( Vector & v, 
 					 const Communicator & column_comm){
@@ -306,7 +184,7 @@ namespace t10 {
 				     data.world.rank(), on_the_top);
 	    }else{
 	      serial::apply_householder_right( beta, v_left, M, column_index);
-	 	serial::apply_householder_left( beta, v_left, M, column_index,); 
+	      serial::apply_householder_left( beta, v_left, M, column_index,); 
 	    }
 	}
 	template< typename Matrix_data>
